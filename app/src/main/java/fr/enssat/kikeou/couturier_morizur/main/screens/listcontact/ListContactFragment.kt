c9d8133d4cb6d5a1e11bf0b734174cfeed5ca3e0
Tristan COUTURIER <tristan.couturier1998@gmail.com>
2021-12-05 22:47:35 +0100

    :sparkles: Read qr code

--- app/src/main/java/fr/enssat/kikeou/couturier_morizur/main/screens/listcontact/ListContactFragment.kt
+++ app/src/main/java/fr/enssat/kikeou/couturier_morizur/main/screens/listcontact/ListContactFragment.kt
@@ -1,19 +1,32 @@
 package fr.enssat.kikeou.couturier_morizur.main.screens.listcontact
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.zxing.qrcode.QRCodeReader
 import fr.enssat.kikeou.couturier_morizur.R
 import fr.enssat.kikeou.couturier_morizur.databinding.FragmentListContactBinding
 import fr.enssat.kikeou.couturier_morizur.databinding.FragmentMainContactBinding
+import fr.enssat.kikeou.couturier_morizur.qrcode.QrCodeActivity
+import fr.enssat.kikeou.couturier_morizur.qrcode.ReadQrCodeActivity
+import fr.enssat.kikeou.couturier_morizur.qrcode.ReadQrCodeContract
+import fr.enssat.kikeou.couturier_morizur.welcome.WelcomeActivityContract
 
 class ListContactFragment : Fragment() {
 
     private var _binding: FragmentListContactBinding? = null
     private val binding get() = _binding!!
+
+    private val startForResult = registerForActivityResult(ReadQrCodeContract()) {
+        // TODO: Parse json to Contact and Locations
+        Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +40,7 @@ class ListContactFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.scanQrButton.setOnClickListener{
-            Log.e("aloha", "coucou")
+            startForResult.launch(null)
         }
     }
 }
--- app/src/main/java/fr/enssat/kikeou/couturier_morizur/qrcode/ReadQrCodeActivity.kt
+++ app/src/main/java/fr/enssat/kikeou/couturier_morizur/qrcode/ReadQrCodeActivity.kt
@@ -1,27 +1,124 @@
 package fr.enssat.kikeou.couturier_morizur.qrcode
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.widget.Toast
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
-import fr.enssat.kikeou.couturier_morizur.databinding.ActivityQrCodeBinding
+import com.google.mlkit.vision.common.InputImage
+import fr.enssat.kikeou.couturier_morizur.databinding.ActivityReadQrCodeBinding
 
 class ReadQrCodeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityQrCodeBinding
+    private val REQUEST_CODE = 10;
+
+    private lateinit var binding: ActivityReadQrCodeBinding
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    startCamera()
+                } else {
+                    Toast.makeText(this, "Permission not granted !", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityQrCodeBinding.inflate(layoutInflater)
+        binding = ActivityReadQrCodeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            val permissions = arrayOf(android.Manifest.permission.CAMERA)
+            ActivityCompat.requestPermissions(this,permissions, REQUEST_CODE)
+        }
+    }
+
+    private fun startCamera() = binding.cameraPreview.post {
         val barcodeScanner: BarcodeScanner = BarcodeScanning.getClient(
             BarcodeScannerOptions.Builder()
                 .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
                 .build()
+        )
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener( {
+
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .build()
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(Size(1080, 2340))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+            imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this),
+                @androidx.camera.core.ExperimentalGetImage { imageProxy ->
+                    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                    val image = imageProxy.image
+                    if(image != null) {
+                        val processImage = InputImage.fromMediaImage(image, rotationDegrees)
+                        barcodeScanner.process(processImage).addOnFailureListener {
+                            Log.e("ScannerActivity", "Error: $it.message")
+                            imageProxy.close()
+                        }.addOnSuccessListener { barcodes ->
+                            var rawValue = ""
+                            for (it in barcodes) {
+                                if (rawValue != it.rawValue) {
+                                    rawValue = it.rawValue
+                                    var data = Intent()
+                                    data.putExtra("jsonData", it.rawValue)
+                                    setResult(Activity.RESULT_OK, data)
+                                    finish()
+                                }
+                            }
+                            imageProxy.close()
+                        }
+                    }
+                })
+
+            // Create a new camera selector each time, enforcing lens facing
+            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            // Apply declared configs to CameraX using the same lifecycle owner
+            cameraProvider.unbindAll()
+            val camera = cameraProvider.bindToLifecycle(
+                this as LifecycleOwner, cameraSelector, preview, imageAnalysis)
+
+            // Use the camera object to link our preview use case with the view
+            preview.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+        },
+            ContextCompat.getMainExecutor(this)
         )
     }
 }
