7f1f880901ad5368c4a4abea98d4e4223d7c6308
Xenilov <tristan.couturier1998@gmail.com>
2021-12-07 14:56:35 +0100

    :sparkles: Add it to details contact

--- app/src/main/java/fr/enssat/kikeou/couturier_morizur/main/ContactDetailsViewModel.kt
+++ app/src/main/java/fr/enssat/kikeou/couturier_morizur/main/ContactDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package fr.enssat.kikeou.couturier_morizur.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import fr.enssat.kikeou.couturier_morizur.database.repository.ContactRepository
 import fr.enssat.kikeou.couturier_morizur.database.repository.LocationRepository
@@ -8,5 +9,22 @@ class ContactDetailsViewModel(
     userId: String,
     contactRepository: ContactRepository,
     private val locationRepository: LocationRepository): ViewModel() {
+        val selectedWeek = MutableLiveData<Int>(1)
         var contact = contactRepository.getById(userId)
+
+    fun nextWeek() {
+        if(selectedWeek.value == 53){
+            selectedWeek.value = 1
+        } else {
+            selectedWeek.value = selectedWeek.value?.plus(1)
+        }
+    }
+
+    fun previousWeek() {
+        if(selectedWeek.value == 1){
+            selectedWeek.value = 53
+        } else {
+            selectedWeek.value = selectedWeek.value?.minus(1)
+        }
+    }
     }
